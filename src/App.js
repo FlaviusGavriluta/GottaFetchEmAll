031206dba9530431926330f1b02758c50355d551
ChircuAndrei <chircu.andrei93@gmail.com>
2023-03-20 15:09:29 +0200

    1st Version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { OnLocations } from './Components/Locations.js';
+
 
 function App() {
+
+
+  const [locations, setLocations] = useState([]);
+
+
+
+  useEffect(() => {
+    (async function() {
+        try {
+            const response = await fetch(
+                'https://pokeapi.co/api/v2/location'
+            );
+            const data = await response.json();
+            // console.log(data.results)
+            setLocations(data.results)
+        } catch (e) {
+            console.error(e);
+        }
+    })();
+}, []);
+// console.log(setLocation)
+// console.log(location)
+
+
+
+// location.areas[rand].url ->
+// let area = fetch("selectedLocation")
+// area.pokemon_encounters[rand].pokemon.url
+
   return (
     <div className="App">
-      
+     < OnLocations locations={locations} />
     </div>
   );
 }
